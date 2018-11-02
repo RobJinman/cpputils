60374f52a780db6be064ad75ac2a08bcec6ba74b
Rob Jinman <jinmanr@gmail.com>
2018-11-02 23:03:06 +0000

    Wrote test cases for ContigMultiArray

--- include/contig_multi_array.hpp
+++ include/contig_multi_array.hpp
@@ -1,14 +1,40 @@
-#ifndef __CONTIG_MULTI_ARRAY_HPP__
-#define __CONTIG_MULTI_ARRAY_HPP__
+#ifndef CPPUTILS_CONTIG_MULTI_ARRAY_HPP
+#define CPPUTILS_CONTIG_MULTI_ARRAY_HPP
 
 
 #include <stdexcept>
+#include <cstring>
+#include <initializer_list>
 
 
 template <typename T, size_t D>
 class ContigMultiArray {
+  friend class ContigMultiArray<T, D + 1>;
+
   public:
     T* data;
+    const size_t dimensionality = D;
+
+    ContigMultiArray(std::initializer_list<ContigMultiArray<T, D - 1>> arr) {
+      m_size = new size_t[D];
+
+      m_size[0] = arr.size();
+      memcpy(&m_size[1], arr.begin()->m_size, (D - 1) * sizeof(size_t));
+
+      size_t elements = 1;
+      for (size_t i = 0; i < D; ++i) {
+        elements *= m_size[i];
+      }
+
+      m_stride = computeStride();
+      data = new T[elements];
+
+      size_t slice = 0;
+      for (auto& sub : arr) {
+        memcpy(data + slice * m_stride, sub.data, sub.size() * sizeof(T));
+        ++slice;
+      }
+    }
 
     ContigMultiArray(const size_t size[D])
       : data(nullptr) {
@@ -22,22 +48,39 @@ class ContigMultiArray {
       }
 
       data = new T[t];
+
+      m_stride = computeStride();
     }
 
-    ContigMultiArray(T* data, size_t* size)
-      : data(data),
-        m_size(size) {}
+    ContigMultiArray(T* data, size_t size[D])
+      : data(data) {
 
-    ContigMultiArray<T, D - 1> operator[](size_t i) {
-      if (i >= m_size[0]) {
+      m_size = new size_t[D];
+      memcpy(m_size, size, sizeof(size_t) * D);
+
+      m_stride = computeStride();
+    }
+
+    size_t length() const {
+      return m_size[0];
+    }
+
+    size_t size() const {
+      size_t s = 1;
+      for (size_t i = 0; i < D; ++i) {
+        s *= m_size[i];
+      }
+      return s;
+    }
+
+    ContigMultiArray<T, D - 1> operator[](size_t slice) {
+      if (slice >= m_size[0]) {
         throw std::runtime_error("Subscript out of range");
       }
 
-      for (size_t j = 1; j < D; ++j) {
-        i *= m_size[D - j];
-      }
+      size_t offset = slice * m_stride;
 
-      return ContigMultiArray<T, D - 1>(data + i, &m_size[1]);
+      return ContigMultiArray<T, D - 1>(data + offset, &m_size[1]);
     }
 
     void del() {
@@ -47,24 +90,58 @@ class ContigMultiArray {
 
   private:
     size_t* m_size;
+    size_t m_stride;
+
+    size_t computeStride() const {
+      size_t stride = 1;
+      for (size_t i = 1; i < D; ++i) {
+        stride *= m_size[i];
+      }
+      return stride;
+    }
 };
 
 template<typename T>
 class ContigMultiArray<T, 1> {
+  friend class ContigMultiArray<T, 2>;
+
   public:
     T* data;
 
-    ContigMultiArray(size_t* size) {
-      m_size = *size;
-      data = new T[m_size];
+    ContigMultiArray(std::initializer_list<T> elems) {
+      data = new T[elems.size()];
+
+      int i = 0;
+      for (auto& e : elems) {
+        data[i] = e;
+        ++i;
+      }
+
+      m_size = new size_t[1];
+      m_size[0] = elems.size();
     }
 
-    ContigMultiArray(T* data, size_t* size)
-      : data(data),
-        m_size(*size) {}
+    ContigMultiArray(size_t size[1]) {
+      m_size[0] = *size;
+      data = new T[m_size[0]];
+    }
+
+    ContigMultiArray(T* data, size_t size[1])
+      : data(data) {
+
+      memcpy(m_size, size, sizeof(size_t));
+    }
+
+    size_t length() const {
+      return m_size[0];
+    }
+
+    size_t size() const {
+      return length();
+    }
 
     T& operator[](size_t i) {
-      if (i >= m_size) {
+      if (i >= m_size[0]) {
         throw std::runtime_error("Subscript out of range");
       }
 
@@ -73,10 +150,11 @@ class ContigMultiArray<T, 1> {
 
     void del() {
       delete[] data;
+      delete[] m_size;
     }
 
   private:
-    size_t m_size;
+    size_t* m_size;
 };
 
 
--- test/src/contig_multi_array_test.cpp
+++ test/src/contig_multi_array_test.cpp
@@ -5,11 +5,99 @@
 class ContigMultiArrayTest : public testing::Test {
   public:
     virtual void SetUp() override {}
-
     virtual void TearDown() override {}
 };
 
 
-TEST_F(ContigMultiArrayTest, foo) {
-  ASSERT_EQ(5, 4 + 1);
+typedef ContigMultiArray<int, 1> IntArr1;
+typedef ContigMultiArray<int, 2> IntArr2;
+typedef ContigMultiArray<int, 3> IntArr3;
+
+
+template<typename T, size_t D>
+bool containsContiguously(const ContigMultiArray<T, D>& arr, std::initializer_list<T> values) {
+  int i = 0;
+  for (auto& val : values) {
+    if (val != arr.data[i]) {
+      return false;
+    }
+    ++i;
+  }
+  return true;
+}
+
+TEST_F(ContigMultiArrayTest, oneDimensional) {
+  IntArr1 arr({0, 1, 2, 3, 4});
+
+  ASSERT_EQ(5, arr.length());
+  ASSERT_EQ(0, arr[0]);
+  ASSERT_EQ(1, arr[1]);
+  ASSERT_EQ(2, arr[2]);
+  ASSERT_EQ(3, arr[3]);
+  ASSERT_EQ(4, arr[4]);
+
+  arr.del();
+}
+
+TEST_F(ContigMultiArrayTest, twoDimensional) {
+  IntArr2 arr({
+    {0, 1, 2, 3},
+    {4, 5, 6, 7},
+    {8, 9, 0, 1}});
+
+  bool contiguous = containsContiguously<int, 2>(arr, {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1});
+  ASSERT_TRUE(contiguous);
+
+  ASSERT_EQ(0, arr[0][0]);
+  ASSERT_EQ(1, arr[0][1]);
+  ASSERT_EQ(2, arr[0][2]);
+  ASSERT_EQ(3, arr[0][3]);
+  ASSERT_EQ(4, arr[1][0]);
+  ASSERT_EQ(5, arr[1][1]);
+  ASSERT_EQ(6, arr[1][2]);
+  ASSERT_EQ(7, arr[1][3]);
+  ASSERT_EQ(8, arr[2][0]);
+  ASSERT_EQ(9, arr[2][1]);
+  ASSERT_EQ(0, arr[2][2]);
+  ASSERT_EQ(1, arr[2][3]);
+
+  arr.del();
+}
+
+TEST_F(ContigMultiArrayTest, threeDimensional) {
+  IntArr3 arr({
+    {{0, 1}, {2, 3}, {4, 5}, {6, 7}},
+    {{8, 9}, {0, 1}, {2, 3}, {4, 5}},
+    {{6, 7}, {8, 9}, {0, 1}, {2, 3}}});
+
+  bool contiguous = containsContiguously<int, 3>(arr,
+    {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3});
+  ASSERT_TRUE(contiguous);
+
+  ASSERT_EQ(0, arr[0][0][0]);
+  ASSERT_EQ(1, arr[0][0][1]);
+  ASSERT_EQ(2, arr[0][1][0]);
+  ASSERT_EQ(3, arr[0][1][1]);
+  ASSERT_EQ(4, arr[0][2][0]);
+  ASSERT_EQ(5, arr[0][2][1]);
+  ASSERT_EQ(6, arr[0][3][0]);
+  ASSERT_EQ(7, arr[0][3][1]);
+  ASSERT_EQ(8, arr[1][0][0]);
+  ASSERT_EQ(9, arr[1][0][1]);
+  ASSERT_EQ(0, arr[1][1][0]);
+  ASSERT_EQ(1, arr[1][1][1]);
+  ASSERT_EQ(2, arr[1][2][0]);
+  ASSERT_EQ(3, arr[1][2][1]);
+  ASSERT_EQ(4, arr[1][3][0]);
+  ASSERT_EQ(5, arr[1][3][1]);
+  ASSERT_EQ(6, arr[2][0][0]);
+  ASSERT_EQ(7, arr[2][0][1]);
+  ASSERT_EQ(8, arr[2][1][0]);
+  ASSERT_EQ(9, arr[2][1][1]);
+  ASSERT_EQ(0, arr[2][2][0]);
+  ASSERT_EQ(1, arr[2][2][1]);
+  ASSERT_EQ(2, arr[2][3][0]);
+  ASSERT_EQ(3, arr[2][3][1]);
+
+  arr.del();
 }
